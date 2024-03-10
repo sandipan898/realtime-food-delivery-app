e65f4b3a240948af51093dd269e755e1f672c20d
sandipan898 <sandipan.das898@gmail.com>
2024-03-11 00:12:04 +0530

    add cart and auth page

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3300
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 // Assets
 app.use(express.static('public'))
 
@@ -18,6 +14,24 @@ app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs')
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 
 app.listen(PORT, () => {
