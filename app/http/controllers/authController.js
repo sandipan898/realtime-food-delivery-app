62c519c91e545b263bbae892436b4fa476adf5f8
sandipan898 <sandipan.das898@gmail.com>
2024-06-23 12:35:26 +0530

    final commit

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -6,6 +6,26 @@ function authController() {
     const _getRedirectUrl = (req) => {
         return req.user.role === 'admin' ? '/admin/orders' : '/orders'
     }
+
+    const mergeCarts = (guestCart, userCart) => {
+        if (!userCart.items) {
+            userCart.items = {};
+        }
+    
+        for (const [id, item] of Object.entries(guestCart.items)) {
+            if (userCart.items[id]) {
+                userCart.items[id].qty += item.qty;
+            } else {
+                userCart.items[id] = item;
+            }
+        }
+    
+        userCart.totalQty = (userCart.totalQty || 0) + guestCart.totalQty;
+        userCart.totalPrice = (userCart.totalPrice || 0) + guestCart.totalPrice;
+    
+        return userCart;
+    }
+
     return {
         login(req, res) {
             res.render('auth/login')
@@ -18,6 +38,9 @@ function authController() {
                 req.flash('error', 'All fields are required!')
                 return res.redirect('/login')
             }
+
+            const guestCart = req.session.cart;
+            
             passport.authenticate('local', (err, user, info) => {
                 if(err) {
                     req.flash('error', info.message)
@@ -31,6 +54,9 @@ function authController() {
                     if(err) {
                         req.flash('error', info.message)
                         return next(err)
+                    }
+                    if (guestCart) {
+                        req.session.cart = mergeCarts(guestCart, req.session.cart || {});
                     }
                     return res.redirect(_getRedirectUrl(req))
                 })
@@ -71,6 +97,11 @@ function authController() {
 
             user.save().then(user => {
                 // Login
+                // Merge guest cart with user cart after registration
+                const guestCart = req.session.cart;
+                if (guestCart) {
+                    req.session.cart = mergeCarts(guestCart, req.session.cart || {});
+                }
                 return res.redirect('/')
             }).catch(err => {
                 req.flash('error', 'Something went wrong!')
